74d9ed0724b588bca1f050f7eadb9f8586281103
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-03 07:20:24 -0700

    added EmployeeTitles.css and updated Heading.js file

---
+++ employee-directory/src/components/Heading/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Heading() {
+    return (
+        <div className="bg-primary">
+            <h3>Employee Directory</h3>
+            <h6>Use Search Box for quick access</h6>
+            
+        </div>
+    )
+}
